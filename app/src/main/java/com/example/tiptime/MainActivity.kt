0512c9a9c70ca80bfd67fc7a19d489f35c3b77a2
Lakshmi Nunna <nunna8021@students.d211.org>
2024-09-11 08:20:00 -0500

    Made state variables stateless to access in other functions
    Pulled the state variables to its ancestor, TipTimeLayout to allow easy use

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -45,6 +47,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember {mutableStateOf("")}
+    val amount = amountInput.toDoubleOrNull()?: 0.0
+    val tip = calculateTip(amount)
     Surface(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -62,7 +67,13 @@ fun TipTimeLayout() {
                     .padding(bottom = 16.dp, top = 40.dp)
                     .align(alignment = Alignment.Start)
             )
-            EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+            EditNumberField(
+                value = amountInput,
+                onValueChange = { amountInput = it },
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+            )
             Text(
                 text = stringResource(R.string.tip_amount, "$0.00"),
                 style = MaterialTheme.typography.displaySmall
@@ -72,11 +83,18 @@ fun TipTimeLayout() {
     }
 }
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput by remember {mutableStateOf("0")}
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
     TextField(
-        value = amountInput.value,
-        onValueChange = { amountInput.value = it },
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(stringResource(R.string.bill_amount)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
 }
